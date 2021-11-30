ca91fd658ce912e0b65d17ee095c0edf009b4b5f
Felipe Schirmann <felipeschirmann@gmail.com>
2021-11-30 07:56:55 -0300

    Tests corrected and renamed

--- src/test/entities/FinancingTests.java
+++ src/test/entities/FinancingTests.java
@@ -8,20 +8,23 @@ import entities.Financing;
 public class FinancingTests {
 
 	@Test
-	public void financingShouldCreateObjectWhenDataIsCorrect() {
+	public void contructorShouldCreateObjectWhenDataIsValid() {
 		Financing obj = new Financing(100000.00, 4000.00, 40);
-		Assertions.assertEquals(Financing.class, obj.getClass());
+		
+		Assertions.assertEquals(obj.getTotalAmount(), 100000.00);
+		Assertions.assertEquals(obj.getIncome(), 4000.00);
+		Assertions.assertEquals(obj.getMonths(), 40);
 	}
 	
 	@Test
-	public void financingShouldThrowExecptionWhenDataIsIncorrect() {
+	public void contructorShouldThrowIllegalArgumentExceptionExecptionWhenDataIsInvalid() {
 		Assertions.assertThrows(IllegalArgumentException.class, () -> {
 			Financing obj = new Financing(100000.00, 4000.00, 20);
 		});
 	}
 	
 	@Test
-	public void financingShouldUpdateWhenDataAmountIsCorect() {
+	public void setTotalAmountShouldUpdateWhenDataAmountIsCorect() {
 		Financing obj = new Financing(100000.00, 4000.00, 40);
 		Double expected = 80000.00;
 		obj.setTotalAmount(expected);
@@ -29,7 +32,7 @@ public class FinancingTests {
 	}
 	
 	@Test
-	public void financingShouldThrowExecptionWhenDataAmountToUpdateIsIncorect() {
+	public void setTotalAmountShouldThrowExecptionWhenDataAmountToUpdateIsIncorect() {
 		Assertions.assertThrows(IllegalArgumentException.class, () -> {
 			Financing obj = new Financing(100000.00, 4000.00, 40);
 			obj.setTotalAmount(200000.00);
@@ -37,7 +40,7 @@ public class FinancingTests {
 	}
 
 	@Test
-	public void financingShouldUpdateWhenDataIncomeIsCorect() {
+	public void setIncomeShouldUpdateWhenDataIncomeIsCorect() {
 		Financing obj = new Financing(100000.00, 4000.00, 40);
 		Double expected = 50000.00;
 		obj.setIncome(expected);
@@ -45,30 +48,35 @@ public class FinancingTests {
 	}
 
 	@Test
-	public void financingShouldThrowExecptionWhenDataIncomeToUpdateIsIncorect() {
+	public void setIncomeShouldThrowExecptionWhenDataIncomeToUpdateIsIncorect() {
 		Assertions.assertThrows(IllegalArgumentException.class, () -> {
 			Financing obj = new Financing(100000.00, 2000.00, 40);
 		});
 	}
 	
 	@Test
-	public void financingShouldUpdateWhenDataMonthIsCorect() {
+	public void setMonthsShouldUpdateWhenDataMonthIsCorect() {
 		Financing obj = new Financing(100000.00, 4000.00, 40);
 		obj.setMonths(50);
 		Assertions.assertEquals(obj.getMonths(), 50);
 	}
 
 	@Test
-	public void financingShouldThrowExecptionWhenDataMonthToUpdateIsIncorect() {
+	public void setMonthsShouldThrowExecptionWhenDataMonthToUpdateIsIncorect() {
 		Assertions.assertThrows(IllegalArgumentException.class, () -> {
 			Financing obj = new Financing(100000.00, 2000.00, 20);
 		});
 	}
 	
 	@Test
-	public void financingShouldCalculateEntryWhenDataIsCorect() {
+	public void entryShouldCalculateEntryWhenDataIsCorect() {
 		Financing obj = new Financing(100000.00, 4000.00, 40);
-		Assertions.assertEquals(obj.entry(), 20000);
+		Assertions.assertEquals(obj.entry(), 20000.00);
 	}
 
+	@Test
+	public void quotaShouldCalculateQuotaWhenDataIsCorect() {
+		Financing obj = new Financing(100000.00, 4000.00, 40);
+		Assertions.assertEquals(obj.quota(), 2000.00);
+	}
 }
